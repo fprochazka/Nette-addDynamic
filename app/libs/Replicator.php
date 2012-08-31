7c70b2aebe515c034106c0eab9c6bf3c362ae438
Filip Procházka <hosiplan@gmail.com>
2012-09-01 01:41:22 +0200

    Working replicator

---
+++ app/libs/Replicator.php
@@ -0,0 +1,3 @@
+<?php
+
+throw new \Exception("Download the replicator from https://github.com/Kdyby/Framework/blob/master/libs/Kdyby/Forms/Containers/Replicator.php");
--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -1,4 +1,7 @@
 <?php
+use Nette\Forms\Container;
+use Nette\Application\UI\Form;
+use Nette\Forms\Controls\SubmitButton;
 
 /**
  * Homepage presenter.
@@ -6,9 +9,87 @@
 class HomepagePresenter extends BasePresenter
 {
 
+	protected function startup()
+	{
+		parent::startup();
+		Kdyby\Forms\Containers\Replicator::register();
+	}
+
+
+
+	public function actionDefault()
+	{
+		if ($values = $this->getSession('values')->users) {
+			$this['myForm']->setDefaults($values);
+		}
+	}
+
+
+
 	public function renderDefault()
 	{
-		$this->template->anyVariable = 'any value';
+		$this->template->users = $this->getSession('values')->users;
+	}
+
+
+
+	/**
+	 * @return Form
+	 */
+	protected function createComponentMyForm($name)
+	{
+		$form = new Form;
+
+		$presenter = $this;
+		$invalidateCallback = function () use ($presenter) {
+			/** @var \Nette\Application\UI\Presenter $presenter */
+			$presenter->invalidateControl('usersForm');
+		};
+
+		// jméno, továrnička, výchozí počet
+		$replicator = $form->addDynamic('users', function (Container $container) use ($invalidateCallback) {
+			$container->currentGroup = $container->form->addGroup('člověk', FALSE);
+			$container->addText('name', 'Jméno');
+			$container->addText('email', 'Email')->setRequired();
+
+			$container->addSubmit('remove', 'Smazat')
+				->addRemoveOnClick($invalidateCallback);
+
+			$container->addSubmit('removeAjax', 'Smazat Ajaxem')
+				->setAttribute('class', 'ajax')
+				->addRemoveOnClick($invalidateCallback);
+		}, 1);
+
+		/** @var \Kdyby\Forms\Containers\Replicator $replicator */
+		$replicator->addSubmit('add', 'Přidat dalšího')
+			->addCreateOnClick($invalidateCallback);
+
+		$replicator->addSubmit('addAjax', 'Přidat dalšího Ajaxem')
+			->setAttribute('class', 'ajax')
+			->addCreateOnClick($invalidateCallback);
+
+		$form->addSubmit('send', 'Zpracovat')
+			->onClick[] = callback($this, 'MyFormSubmitted');
+
+		$form->addSubmit('sendAjax', 'Zpracovat Ajaxem')
+			->setAttribute('class', 'ajax')
+			->onClick[] = callback($this, 'MyFormSubmitted');
+
+		$this[$name] = $form;
+		$form->action .= '#snippet--usersForm';
+		return $form;
+	}
+
+
+
+	/**
+	 * @param SubmitButton $button
+	 */
+	public function MyFormSubmitted(SubmitButton $button)
+	{
+		// jenom naplnění šablony, bez přesměrování
+		$this->getSession('values')->users = $button->form->values;
+		$this->redirect('this');
 	}
 
 }
